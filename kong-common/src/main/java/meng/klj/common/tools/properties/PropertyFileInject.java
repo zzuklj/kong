141d12fbc4e8b5b6a5d499d0224a742399b422da
klj <konglj@jxgtmbjt.com>
2019-12-10 23:41:00 +0800

    init

--- kong-common/src/main/java/meng/klj/common/tools/properties/PropertyFileInject.java
+++ kong-common/src/main/java/meng/klj/common/tools/properties/PropertyFileInject.java
@@ -1,15 +1,29 @@
 package meng.klj.common.tools.properties;
 
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.util.Arrays;
 import java.util.Properties;
 
+@Slf4j
 public class PropertyFileInject {
 
     private Properties properties;
 
     private PropertyNameMapper propertyNameMapper;
 
-    public PropertyFileInject(ClassLoader cl, String... properttFiles){
+    public PropertyFileInject(ClassLoader cl, String... propertyFiles){
         properties = new Properties();
         propertyNameMapper = PropertyNameMapper.UnderLine2Camel;
+        Arrays.stream(propertyFiles).forEach(p -> {
+            try {
+                properties.load(new InputStreamReader(cl.getResourceAsStream(p), Charset.forName("utf-8")));
+            } catch (IOException e) {
+                log.warn("cannot read property file: {}", p);
+            }
+        });
     }
 }
