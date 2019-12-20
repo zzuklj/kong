66d22a0410529772c2cfb0727c2885320a84f0a5
Kong LingJun <konglj@jxgtmbjt.com>
2019-12-20 09:42:29 +0800

    aop test

---
+++ kong-upms-impl/src/main/java/meng/klj/upms/KongConfigApplication.java
@@ -0,0 +1,17 @@
+package meng.klj.upms;
+
+import meng.klj.upms.test.IUserTestService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@ComponentScan({"meng.klj.upms.test"})
+@EnableAspectJAutoProxy
+public class KongConfigApplication {
+
+    public static void main(String[] args) {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(KongConfigApplication.class);
+        IUserTestService userTestService = ac.getBean(IUserTestService.class);
+        userTestService.getById(1L);
+    }
+}
