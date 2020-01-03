70f81b119288bc86abae0a8abcee265122f34de1
Kong LingJun <konglj@jxgtmbjt.com>
2020-01-03 18:02:45 +0800

    ..

--- kong-common/src/main/java/meng/klj/common/little/java8/KongTest.java
+++ kong-common/src/main/java/meng/klj/common/little/java8/KongTest.java
@@ -1,9 +1,19 @@
 package meng.klj.common.little.java8;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import meng.klj.common.tools.dataintegrity.TestEntity;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
+import org.springframework.core.io.ClassPathResource;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Properties;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class KongTest {
@@ -29,5 +39,15 @@ public class KongTest {
 
     static void countCharNum(String c){
         strCountMap.computeIfAbsent(c, (key) -> new AtomicInteger()).incrementAndGet();
+    }
+
+    @Test
+    public void streamGo() throws IOException {
+        File f = new ClassPathResource("test.json").getFile();
+        String s = FileUtils.readFileToString(f);
+        ObjectMapper mapper = new ObjectMapper();
+        TestEntity testEntity = mapper.readValue(s, TestEntity.class);
+        String id = testEntity.getId();
+
     }
 }
