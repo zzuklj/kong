156ccb0613bd74c8f92f5771855066b79cd268df
klj <konglj@jxgtmbjt.com>
2020-01-12 22:35:26 +0800

    init

--- kong-common/src/main/java/meng/klj/common/base/BaseController.java
+++ kong-common/src/main/java/meng/klj/common/base/BaseController.java
@@ -1,8 +1,15 @@
 package meng.klj.common.base;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
 
 @Slf4j
 public abstract class BaseController {
 
+    private static final String startLogerTemplate = "{}-begin-operateBy-{}>>>param: {}";
+    private static final String endLogerTemplate = "{}-end<<<return: result={}";
+git
+    private static Cache<Class, Logger> loggerCache = CacheBuilder.newBuilder().build();
 }
