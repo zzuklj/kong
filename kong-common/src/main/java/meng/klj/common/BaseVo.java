a8240cd3cf4239d0f72aeb2822febd63f475250e
klj <konglj@jxgtmbjt.com>
2021-04-18 17:38:22 +0800

    test

--- kong-common/src/main/java/meng/klj/common/BaseVo.java
+++ kong-common/src/main/java/meng/klj/common/BaseVo.java
@@ -1,4 +1,15 @@
 package meng.klj.common;
 
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+
 public class BaseVo {
+    public static void main(String[] args) {
+        LinkedHashSet<String> linkedHashSet = new LinkedHashSet<>();
+        linkedHashSet.add("123");
+        linkedHashSet.add("456");
+
+        LinkedHashMap<Object, Object> linkedHashMap = new LinkedHashMap<>();
+        linkedHashMap.put(1,1);
+    }
 }
---
+++ kong-common/src/main/java/meng/klj/common/reactor/siglethread/Handler.java
@@ -0,0 +1,9 @@
+package meng.klj.common.reactor.siglethread;
+
+import java.nio.channels.Selector;
+import java.nio.channels.SocketChannel;
+
+public class Handler {
+    public Handler(Selector selector, SocketChannel c) {
+    }
+}
